33ab2c7472da466f004d194b5828399ef27169fd
Davdi <n.davdi@gmail.com>
2020-12-01 13:22:34 +0100

    1.3

--- calculator.js
+++ calculator.js
@@ -1,25 +1,45 @@
 const calcs = {
-    '+': function(a,b) { return a + b },
-    '-': function(a,b) { return a - b },
-    '÷': function(a,b) { return a / b },
-    'x': function(a,b) { return a * b }
+    '+': function (a, b) { return a + b },
+    '-': function (a, b) { return a - b },
+    '/': function (a, b) { return a / b },
+    '*': function (a, b) { return a * b }
 }
 
-let calcInput = '';
-calcInput += document.getElementsByTagName("INPUT");
-const validSigns = ['+', '-', '÷', 'x'];
+// const signs = ['+'];
+
+// for (let i = 0; i < signs.length; i += 1) {
+//     console.log(calcs[signs[i]](10, 20));
+// }
+
+function handleClick() {
+    const keys = document.querySelectorAll('.btn-number');
+    keys.forEach(key => {
+        key.addEventListener('click', () => {
+            writeOutput(event.target.dataset.value);
+        })
+    })
+}
+
+function writeOutput(value) {
+    document.querySelector('input-field').value += value;
+}
+
+let calcString = '';
+// calcString += '23+45.45-34';
+const acceptableSigns = ['+', '-', '/', '*']
 const nums = [];
 const signs = [];
 
-
-function fillInput() {
-    // document.getElementsByTagName("INPUT")[0].setAttribute("value", "próba");
+let num = '';
+for (let i = 0; i < calcString.length; i += 1) {
+    if (acceptableSigns.indexOf(calcString[i]) === -1) {
+        num += calcString[i];
+    }
+    else {
+        nums.push(parseFloat(num));
+        num = '';
+        signs.push(calcString[i])
+    }
 }
-
-function clearInput() {
-    document.getElementsByTagName("INPUT")[0].setAttribute("value", "");
-}
-
-for (let i = 0; i < signs.length; i += 1) {
-    console.log(calcs[signs[i]](10, 20));
-}
+nums.push(parseFloat(num));
+console.log(nums, signs);
