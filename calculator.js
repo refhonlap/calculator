85f253c0500abaf417576c2deb95f796c257c9a2
Davdi <n.davdi@gmail.com>
2020-12-01 07:58:57 +0100

    1.2

---
+++ calculator.js
@@ -0,0 +1,7 @@
+// const calcs = {
+//     '+': function(a,b) { return a + b },
+//     '-': function(a,b) { return a - b },
+//     '÷': function(a,b) { return a / b },
+//     'x': function(a,b) { return a * b },
+// }
+
