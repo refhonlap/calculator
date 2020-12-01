db00d5c51d38f005ba0920105fed13ca694b8a15
Davdi <n.davdi@gmail.com>
2020-12-01 12:38:50 +0100

    1.21

--- calculator.js
+++ calculator.js
@@ -1,7 +1,25 @@
-// const calcs = {
-//     '+': function(a,b) { return a + b },
-//     '-': function(a,b) { return a - b },
-//     '÷': function(a,b) { return a / b },
-//     'x': function(a,b) { return a * b },
-// }
+const calcs = {
+    '+': function(a,b) { return a + b },
+    '-': function(a,b) { return a - b },
+    '÷': function(a,b) { return a / b },
+    'x': function(a,b) { return a * b }
+}
 
+let calcInput = '';
+calcInput += document.getElementsByTagName("INPUT");
+const validSigns = ['+', '-', '÷', 'x'];
+const nums = [];
+const signs = [];
+
+
+function fillInput() {
+    // document.getElementsByTagName("INPUT")[0].setAttribute("value", "próba");
+}
+
+function clearInput() {
+    document.getElementsByTagName("INPUT")[0].setAttribute("value", "");
+}
+
+for (let i = 0; i < signs.length; i += 1) {
+    console.log(calcs[signs[i]](10, 20));
+}
